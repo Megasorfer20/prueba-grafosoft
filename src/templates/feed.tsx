3aca3674bbbc5487b562cff8e83b1020bb9409de
Esteban Dario Ruiz <edrp.campus15@gmail.com>
2024-02-09 13:02:45 -0500

    basic template and proyect creation

---
+++ src/templates/feed.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/feed.css';
+
+function Feed() {
+  return (
+    <div className="mainfeed">
+      
+    </div>
+  );
+}
+
+export default Feed;
---
+++ src/templates/navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/navbar.css';
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      
+    </div>
+  );
+}
+
+export default Navbar;
